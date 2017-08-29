8533ff2a317de4bd060ca9391900f336381ab997
winar-jin <winar1994@outlook.com>
2017-08-29 19:42:49 +0800

    format code

--- main/section-1/practice-1.js
+++ main/section-1/practice-1.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  if(!collectionA || !collectionB || !Array.isArray(collectionA) || !Array.isArray(collectionB)){
+  if (!collectionA || !collectionB || !Array.isArray(collectionA) || !Array.isArray(collectionB)) {
     return;
   }
   let result = [];
   collectionA.forEach(item => {
-    if(collectionB.includes(item)){
+    if (collectionB.includes(item)) {
       result.push(item);
     }
   });
   return result;
-}
+};
--- main/section-3/practice-1.js
+++ main/section-3/practice-1.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  if(!collectionA || !objectB || !Array.isArray(collectionA) || Object.keys(objectB).length === 0){
+  if (!collectionA || !objectB || !Array.isArray(collectionA) || Object.keys(objectB).length === 0) {
     return;
   }
   let result = JSON.parse(JSON.stringify(collectionA));
   result.forEach(item => {
-    if(objectB.value.includes(item.key)){
+    if (objectB.value.includes(item.key)) {
       --item.count;
     }
   });
   return result;
-}
+};
